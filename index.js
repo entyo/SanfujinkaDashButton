e9f0f1259646ede89cb90e4df7e719219e01e1d7
e_ntyo <entyo.contact@gmail.com>
2016-12-17 01:46:54 +0900

    :pencil2: slackへの投稿用モジュールをリファクタリング

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+"use strict";
 const dash_button = require('node-dash-button');
-const slack = require('./slack.js');
+const Slack = require('./slack.js');
 
 // DashボタンのMACアドレス
 const dash = dash_button('88:71:e5:3f:34:b0', null, null, 'all');
@@ -7,6 +8,7 @@ const dash = dash_button('88:71:e5:3f:34:b0', null, null, 'all');
 // ボタンをクリックしたときのアクション
 // クリックしてから数秒遅延する
 dash.on('detected', () => {
+  const slack = new Slack("さんふじんか", "とうきょう ちよだく");
   slack.post();
   // console.log("Pushed!");
 });
--- slack.js
+++ slack.js
@@ -1,36 +1,39 @@
+"use strict";
 const request = require('request')
 
-var options = {
-  uri: 'https://hooks.slack.com/services/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',
-  headers: { 
-    'Content-Type': 'application/json' 
-  },
-  json: {
-    attachments: [
-      {
-        fallback: "最寄りの産婦人科情報",
-        color: '#ED225D',
-        title: '中野産婦人科医院', // name
-        fields: [
+class Slack {
+  constructor(name, address) {
+    this.options = {
+      uri: 'https://hooks.slack.com/services/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',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      json: {
+        attachments: [
           {
-            "value":"日本, 〒187-0025 東京都小平市津田町１丁目４−８", // formatted address
-            "short":false
+            fallback: "最寄りの産婦人科情報",
+            color: '#ED225D',
+            title: name,
+            fields: [
+              {
+                "value": address,
+                "short": false
+              }
+            ]
           }
         ]
       }
-    ]
+    };
   }
-};
-
-module.exports = {
-  post: function() {
-    request.post(options, function(error, response, body){
+  post(){
+    request.post(this.options, function (error, response, body) {
       if (error || response.statusCode != 200) {
-        console.error('error: '+ response.statusCode + '\n' + response.body);
-      }
-      else {
+        console.error('error: ' + response.statusCode + '\n' + response.body);
+      } else {
         console.log(body);
       }
     });
   }
 };
+
+module.exports = Slack;
