6c9024c66d03c4ba9f8711ed2c822df3ce8870dc
e_ntyo <entyo.contact@gmail.com>
2016-12-17 13:47:58 +0900

    :pencil2: Slack投稿用モジュールをリファクタリング

--- slack.js
+++ slack.js
@@ -2,29 +2,27 @@
 const request = require('request')
 
 class Slack {
-  constructor(name, address) {
+
+  constructor(uri, name, address) {
     this.options = {
-      uri: 'https://hooks.slack.com/services/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',
+      uri: uri,
       headers: {
         'Content-Type': 'application/json'
       },
       json: {
-        attachments: [
-          {
-            fallback: "最寄りの産婦人科情報",
-            color: '#ED225D',
-            title: name,
-            fields: [
-              {
-                "value": address,
-                "short": false
-              }
-            ]
-          }
-        ]
+        attachments: [{
+          fallback: "最寄りの産婦人科情報",
+          color: '#ED225D',
+          title: name,
+          fields: [{
+            "value": address,
+            "short": false
+          }]
+        }]
       }
     };
   }
+
   post(){
     request.post(this.options, function (error, response, body) {
       if (error || response.statusCode != 200) {
@@ -34,6 +32,7 @@ class Slack {
       }
     });
   }
+
 };
 
 module.exports = Slack;
