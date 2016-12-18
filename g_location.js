0f6f72c97a33053f7c90639e5d2224bf7b4c6120
e_ntyo <entyo.contact@gmail.com>
2016-12-18 20:44:21 +0900

    :pencil2: 産婦人科情報の取得→Slackへの投稿 まで一本でできるようにした

--- g_location.js
+++ g_location.js
@@ -1,27 +1,37 @@
 "use strict";
-const request = require('request')
 
 class GLocation {
 
   constructor(key, query, lon, lat, radius, sortBy) {
 
     this.url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
-                + "key=" + key
-                + "&query=" + encodeURIComponent(query)
-                + "&location=" + lon + ',' + lat // [緯度, 経度]
-                + "&radius=" + radius; // [メートル]
-                + "&rankby=" + sortBy // prominence(重要度) / distance(近い順)
+                + "key=" + key                          // APIキー
+                + "&query=" + encodeURIComponent(query) // 検索文字列
+                + "&location=" + lon + ',' + lat        // (緯度, 経度)
+                + "&radius=" + radius                   // [メートル]
+                + "&opennow";                           // 営業中
                 
   }
-
+  
   get() {
-    request.get(this.url, function (error, response, body) {
-      if (error || response.statusCode != 200) {
-        console.error('error: ' + response.statusCode + '\n' + response.body);
-      } else {
-        console.log(body);
-      }
-    });
+    const url = this.url;
+    console.log(url);
+    return new Promise(function (resolve, reject) {
+      const request = require('request');
+      request.get(url, function (error, res) {
+        if (error) {
+          reject(new Error(error));
+        }
+        if (res.statusCode === 200) {
+          resolve(res.body);
+        }
+        else {
+          reject(new Error(res.statusCode));
+        }
+      }).on('error', function(e) {
+        reject(new Error(e.message));
+      });
+    })
   }
 
 }
--- index.js
+++ index.js
@@ -15,15 +15,47 @@ const gLocation = new GLocation(
     "distance"
 );
 
-const slack = new Slack(
-  process.env.SLACK_WEBHOOK_URL, 
-  "さんふじんか", 
-  "とうきょう ちよだく"
-);
-
 // ボタンをクリックしたときのアクション
 // クリックしてから数秒遅延する
 dash.on('detected', () => {
-  gLocation.get();
-  slack.post();
+  // Google Location APIで、近くの営業中の産婦人科情報を1件取得する
+  gLocation.get().then(function onFullfilled(res){
+    let place;
+    try {
+      let resObj = JSON.parse(res);
+      place  = resObj["results"][0];
+    }
+    catch (e) {
+      slack = new Slack(
+        process.env.SLACK_WEBHOOK_URL, 
+        "Google Places API のレスポンスのパースに失敗しました。", 
+        e
+      );
+      slack.post();
+    }
+    // Slackに産婦人科情報をPOSTする
+    let slack;
+    try {
+      slack = new Slack(
+        process.env.SLACK_WEBHOOK_URL, 
+        place["name"], 
+        place["formatted_address"]
+      );
+    }
+    catch (e) {
+      slack = new Slack(
+        process.env.SLACK_WEBHOOK_URL, 
+        "営業中の産婦人科の取得に失敗しました…", 
+        "近くに産婦人科がないか、近くの産婦人科は営業を終了しています。"
+      );
+    }
+    slack.post();
+  }).catch(function onRejected(error){
+    slack = new Slack(
+      process.env.SLACK_WEBHOOK_URL, 
+      "Google Places API関係のエラーが発生しました。", 
+      error
+    );
+  });
+
 });
